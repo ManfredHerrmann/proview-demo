3c2323f9170c248474e561beb1ad6dd860f6ea29
Claes Sjofors <claes.sjofors@proview.se>
2014-03-10 16:49:41 +0100

    Inital build script modified

--- src/login/build_from_git.sh
+++ src/login/build_from_git.sh
@@ -3,6 +3,42 @@
 # Build pwrdemo from git clone
 #
 
+nodename=`eval uname -n`
+unamestr=`eval uname`
+onamestr=`eval uname -o`
+machine=`eval uname -m`
+if [ $unamestr == "Darwin" ]; then
+    os="os_macos"
+    hw="hw_x86_64"
+    opsys=256
+elif [ $unamestr == "FreeBSD" ]; then
+    os="os_freebsd"
+    hw="hw_x86_64"
+    opsys=1024
+elif [ $unamestr == "OpenBSD" ]; then
+    os="os_openbsd"
+    hw="hw_x86_64"
+    opsys=2048
+elif [ $onamestr == "Cygwin" ]; then
+    os="os_cygwin"
+    hw="hw_x86"
+    opsys=4096
+else
+    if [ $machine == "x86_64" ]; then
+	machine="x86_64"
+        opsys=128
+    elif [ ${machine:0:3} == "arm" ]; then
+	machine="arm"
+        opsys=512
+    else
+	machine="x86"
+        opsys=64
+    fi
+    os="os_linux"  
+    hw="hw_"$machine
+fi
+
+
 chmod -R u+w $pwrp_root/.git
 
 echo "* Create directories"
@@ -47,15 +83,35 @@ cp $pwrp_load/*.flw $pwrp_web/
 #wb_cmd wb load /load=\"$pwrp_db/directory.wb_dmp\"
 
 echo "*"
-echo "* Edit directory volume"
+echo "* Set nodename $nodename, opsys $opsys"
 echo "*"
-uname -n
-wb pwrp pwrp
+
+cat > initdir.pwr_com <<EOF 
+  set attr/name=Bus999-DemoNode/attr=NodeName/value="$nodename"/noconf
+  set attr/name=Bus999-DemoNode/attr=OperatingSystem/value="$opsys"/noconf
+  save
+  exit
+EOF
+
+wb_cmd @initdir
 
 echo "*"
 echo "* Load volpwrdemo"
 echo "*"
 wb_cmd wb load/nofocode/load=\"$pwrp_db/volpwrdemo.wb_dmp\"/out=\"$pwrp_db/volpwrdemo.lis\"
+
+echo "*"
+echo "* Set opsys $opsys"
+echo "*"
+
+cat > initvol.pwr_com <<EOF 
+  set attr/name=VolPwrDemo:/attr=OperatingSystem/value="$opsys"/noconf
+  save
+  exit
+EOF
+
+wb_cmd -v volpwrdemo @initvol
+
 echo "*"
 echo "* Compile"
 echo "*"
